edbbd8588c51503d8060bade65e2a8555f9bd03e
Eric Butz <ericgbutz@gmail.com>
2025-01-08 13:09:37 -0700

    cleanup

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/ericbutz/git-sandshrew/alkanes-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/ericbutz/git-sandshrew/alkanes-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/ericbutz/git-sandshrew/alkanes-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/ericbutz/git-sandshrew/alkanes-docs/src/css/custom.css"),
-];
