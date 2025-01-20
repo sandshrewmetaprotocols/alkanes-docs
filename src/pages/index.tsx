36c6dd8b1a609c805f850a8bd7180c855d3da697
sprimage <timilehin1999@gmail.com>
2025-01-20 15:53:18 -0600

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import SiteMetadata from '@theme/SiteMetadata';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import HomepageVideo from '@site/src/components/HomepageVideo';
 import styles from './styles.module.css';
@@ -16,6 +17,19 @@ export default function Home(): ReactNode {
         <HomepageVideo />
         <HomepageFeatures />
       </main>
+      <head>
+        <meta property="og:image" content="./static/img/opengraphalkane.jpeg" />
+        <meta
+          property="og:description"
+          content="Discover the Alkanes metaprotocol core docs for learning about smart contracts on Bitcoin, developer guides, and access to the indexer"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="./static/img/opengraphalkane.jpeg" />
+        <meta
+          name="twitter:description"
+          content="Discover the Alkanes metaprotocol core docs for learning about smart contracts on Bitcoin, developer guides, and access to the indexer"
+        />
+      </head>
     </Layout>
   );
 }
