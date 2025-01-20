6770b1633a1f17f33370e4fb1fae9ca4aa401de5
sprimage <timilehin1999@gmail.com>
2025-01-20 17:15:27 -0600

    update docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'Alkanes',
-  tagline: 'The Next Generation of DeFi Infrastructure | Secure, Scalable, and Efficient',
+  tagline: 'Alkanes Docs',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
